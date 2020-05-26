5257c4506ae5512655741e4c336a080a5dad8314
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2020-05-26 20:34:05 +0200

    reorg

---
+++ manage.py
@@ -0,0 +1,17 @@
+from app import app, db
+from flask_migrate import  Migrate
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+
+#db migrate
+#db upgrade
+
+__author__ = 'Augustin Rieunier'
+
+migrate = Migrate(app, db, directory='migrations')
+
+manager =  Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
