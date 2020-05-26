060446a65226457407bd2c205c3c9dc2eeb31218
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2020-05-26 18:31:20 +0200

    version

--- app/model.py
+++ app/model.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-
+    role  = db.Column(db.String(128)) #for later purpose
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
